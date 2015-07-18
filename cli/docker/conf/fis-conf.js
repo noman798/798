c158daca440600154dffdca7bba777f29e3defc0
Noman <noman@798.space>
2015-07-18 09:56:19 +0000

    f

--- cli/docker/conf/fis-conf.js
+++ cli/docker/conf/fis-conf.js
@@ -1,45 +1,6 @@
+require pack
 
-
-fis.config.set('pack', {
-    "pkg/base.js":[
-        "/lib/mod.js",
-        "/lib/jquery.js",
-        "/lib/headroom/headroom.js",
-        "/lib/avalon.modern.shim.js",
-        "/lib/nprogress/nprogress.js",
-        "/lib/director.js",
-        "/lib/jquery.cookie.js",
-        "/lib/semantic/semantic.js",
-        "/lib/config.js",
-        "/lib/json2.js",
-        "/lib/alertify.js",
-        "/lib/scrollbar/jquery.scrollbar.js",
-        "/lib/marked.js",
-        "/lib/autosize.js",
-        "/lib/qrcode.js",
-        "/lib/tageditor/jquery.caret.min.js",
-        "/lib/tageditor/jquery.tag-editor.js",
-        "/lib/AV.realtime.js",
-        "/modules/lib/jquery_ext.js",
-        "/modules/lib/error_tip.js",
-        "/modules/lib/async.js",
-        "/modules/lib/avalon_ext.js",
-        "/modules/lib/modal.js",
-        "/modules/lib/av_ext.js",
-        "/modules/lib/route.js",
-        "/modules/lib/init.js",
-    ],
-
-
-
-    "pkg/base.css":[
-        "/lib/semantic/semantic.css",
-        "/lib/scrollbar/jquery.scrollbar.css",
-        "/lib/headroom/headroom.css",
-        "/lib/nprogress/nprogress.css",
-        "/css/init.css",
-    ]
-});
+fis.config.set('pack', require('pack'));
 
 //静态资源域名，使用pure release命令时，添加--domains或-D参数即可生效
 fis.config.set('roadmap.domain', '//${CONFIG.CDN}');
---
+++ pure/pack.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "pkg/base.js":[
+        "/lib/mod.js",
+        "/lib/jquery.js",
+        "/lib/headroom/headroom.js",
+        "/lib/avalon.modern.shim.js",
+        "/lib/nprogress/nprogress.js",
+        "/lib/director.js",
+        "/lib/jquery.cookie.js",
+        "/lib/semantic/semantic.js",
+        "/lib/config.js",
+        "/lib/json2.js",
+        "/lib/alertify.js",
+        "/lib/scrollbar/jquery.scrollbar.js",
+        "/lib/marked.js",
+        "/lib/autosize.js",
+        "/lib/qrcode.js",
+        "/lib/tageditor/jquery.caret.min.js",
+        "/lib/tageditor/jquery.tag-editor.js",
+        "/lib/AV.realtime.js",
+        "/modules/lib/jquery_ext.js",
+        "/modules/lib/error_tip.js",
+        "/modules/lib/async.js",
+        "/modules/lib/avalon_ext.js",
+        "/modules/lib/modal.js",
+        "/modules/lib/av_ext.js",
+        "/modules/lib/route.js",
+        "/modules/lib/init.js",
+    ],
+
+
+
+    "pkg/base.css":[
+        "/lib/semantic/semantic.css",
+        "/lib/scrollbar/jquery.scrollbar.css",
+        "/lib/headroom/headroom.css",
+        "/lib/nprogress/nprogress.css",
+        "/css/init.css",
+    ]
+
+}
